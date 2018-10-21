e33c6c39c5e7f848f6245a7a7a056a2b94e1dce9
常庭智 <ctz16@mails.tsinghua.edu.cn>
2018-10-21 20:14:08 +0800

    out put first version

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ from scipy import optimize
 from matplotlib import pyplot as plt
 
 framecount = 0
+
+#the right and left edge of central column
 x_colr = 610
 x_coll = 446
 
@@ -13,7 +15,10 @@ def nothing(*argv):
 
 def main():
     cap = cv2.VideoCapture('180907002cap.mp4')
-    R = videoprocess(cap)
+    fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+    out = cv2.VideoWriter('output3.avi', fourcc, 25.0, (1280, 800))
+
+    R = videoprocess(cap,out)
     x = np.linspace(1,framecount,framecount)
 
     i=0
@@ -27,23 +32,25 @@ def main():
     plt.plot(x,R)
     plt.xlabel('frame')
     plt.ylabel('r')
-    plt.ylim(0,500)
+    plt.ylim(0,300)
     plt.show()
 
     cap.release()
-    cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def videoprocess(video):
+def videoprocess(video,writevieo):
     rc=[]
     while(video.isOpened()):
         try:
             ret,frame = video.read()
-            xc,yc,r = imageprocess(frame)
+            xc,yc,r,frame = imageprocess(frame)
             rc.append(r)
             global framecount
             framecount +=1
             print(framecount)
+
+            if ret == True:
+                writevieo.write(frame)
         except:
             break
     return rc
@@ -55,7 +62,7 @@ def imageprocess(img):
 
     ## /thresholding and finding contours/
     mean = np.mean(gray)
-    th = 2.0
+    th = 2.0 - framecount/300
     ths = th*mean
     ret, thresh = cv2.threshold(gray, ths, 255, 0)
     cntimg,contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
@@ -69,26 +76,28 @@ def imageprocess(img):
     leftEdge = findLeftEdge(cnt,img)
     xc_r, yc_r, R_r = circlefit(rightEdge)
     xc_l, yc_l, R_l = circlefit(leftEdge)
-    # cv2.circle(img, (xc_r, yc_r), R_r, (255, 255, 255), 2)
-    # cv2.circle(img, (xc_l, yc_l), R_l, (255, 255, 255), 2)
+    cv2.circle(img, (xc_r, yc_r), R_r, (255, 255, 255), 2)
+    cv2.circle(img, (xc_l, yc_l), R_l, (255, 255, 255), 2)
     # cv2.imshow('img', img)
+    global R_0
     if R_r<R_l:
-        return xc_r,yc_r,R_r
+        return xc_r,yc_r,R_r,img
     else:
-        return xc_l,yc_l,R_l
+        return xc_l,yc_l,R_l,img
 
 ## /histogram equalization/
 def histEqual(im):
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(16,16))
     for i in range(1):
        im = clahe.apply(im)
-    im = cv2.GaussianBlur(im,(9,9),sigmaX=2,sigmaY=2)
+    # im = cv2.GaussianBlur(im,(9,9),sigmaX=2,sigmaY=2)
     return im
 
 ## /find right edge/
 def findRightEdge(cnt,img):
     edge = np.array(cnt)
 
+    #capture the right half contour
     i = 0
     while i < int(edge.size / 2):
         if edge[i][0][0] < x_colr:
@@ -97,28 +106,34 @@ def findRightEdge(cnt,img):
             i += 1
 
     ## /contour ellipse fit/
-    ellipse = cv2.fitEllipse(edge)  # ellipse((x0,y0),(2b,2a),angle),while angle=90,a is horizontal
-    cv2.ellipse(img, ellipse, (255, 0, 0), 1)
+    try:
+        ellipse = cv2.fitEllipse(edge)  # ellipse((x0,y0),(2b,2a),angle),while angle=90,a is horizontal
+        # cv2.ellipse(img, ellipse, (255, 0, 0), 1)
 
-    x0, y0, b, a = int(ellipse[0][0]), int(ellipse[0][1]), int(ellipse[1][0] / 2), int(ellipse[1][1] / 2)
-    xm = 0  # the right edge
-    for x in range(x0, x0 + a - 1):
-        k = -((x - x0) * b * b) / (b * math.sqrt(1 - (x - x0) * (x - x0) / (a * a)) * a * a)
-        if k < -0.8:  # angle = 90
-            xm = x
-            break
+        x0, y0, b, a = int(ellipse[0][0]), int(ellipse[0][1]), int(ellipse[1][0] / 2), int(ellipse[1][1] / 2)
+        xm = 0  # the right edge
+        for x in range(x0, x0 + a - 1):
+            k = -((x - x0) * b * b) / (b * math.sqrt(1 - (x - x0) * (x - x0) / (a * a)) * a * a)
+            if k < -0.8:
+                xm = x
+                break
 
-    i = 0
-    while i < int(edge.size / 2):
-        if edge[i][0][0] < xm:
-            edge = np.delete(edge, i, 0)
-        else:
-            i += 1
+        i = 0
+        while i < int(edge.size / 2):
+            if edge[i][0][0] < xm:
+                edge = np.delete(edge, i, 0)
+            else:
+                i += 1
+    except:
+        print('rightedge not found')
+        return np.zeros_like(cnt)
+
     return edge
 
 def findLeftEdge(cnt,img):
     edge = np.array(cnt)
 
+    # capture the left half contour
     i = 0
     while i < int(edge.size / 2):
         if edge[i][0][0] > x_coll:
@@ -127,24 +142,29 @@ def findLeftEdge(cnt,img):
             i += 1
 
     ## /contour ellipse fit/
-    ellipse = cv2.fitEllipse(edge)  # ellipse((x0,y0),(2b,2a),angle),while angle=90,a is horizontal
-    cv2.ellipse(img, ellipse, (255, 0, 0), 1)
+    try:
+        ellipse = cv2.fitEllipse(edge)  # ellipse((x0,y0),(2b,2a),angle),while angle=90,a is horizontal
+        # cv2.ellipse(img, ellipse, (255, 0, 0), 1)
 
-    x0, y0, b, a = int(ellipse[0][0]), int(ellipse[0][1]), int(ellipse[1][0] / 2), int(ellipse[1][1] / 2)
-    xm = 0  # the right edge
-    for x in range(x0 - a + 1, x0):
-        k = -((x - x0) * b * b) / (b * math.sqrt(1 - (x - x0) * (x - x0) / (a * a)) * a * a)
-        if k < 0.8:  # angle = 90
-            xm = x
-            break
+        x0, y0, b, a = int(ellipse[0][0]), int(ellipse[0][1]), int(ellipse[1][0] / 2), int(ellipse[1][1] / 2)
+        xm = 0  # the right edge
+        for x in range(x0 - a + 1, x0):
+            k = -((x - x0) * b * b) / (b * math.sqrt(1 - (x - x0) * (x - x0) / (a * a)) * a * a)
+            if k < 0.8:
+                xm = x
+                break
 
-    edge = np.array(cnt)
-    i = 0
-    while i < int(edge.size / 2):
-        if edge[i][0][0] > xm:
-            edge = np.delete(edge, i, 0)
-        else:
-            i += 1
+        edge = np.array(cnt)
+        i = 0
+        while i < int(edge.size / 2):
+            if edge[i][0][0] > xm:
+                edge = np.delete(edge, i, 0)
+            else:
+                i += 1
+    except:
+        print('leftedge not found')
+        return np.zeros_like(cnt)
+
     return edge
 
 ## /circle fit/
